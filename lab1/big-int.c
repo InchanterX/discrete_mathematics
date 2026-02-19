8ea633115312e1e6d118f3c6c3de75e6deaf918a
InchanyerX <inchanterx.work@gmail.com>
2026-02-19 23:32:28 +0300

    Init commit, added place for the first labrotory work.

---
+++ lab1/big-int.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SUCCESS 0
+#define MEMORY_ERROR 1
+#define INCORRECT_INPUT 2
+
+int main(void) {
+        printf("Give it a 5, please.\n");
+        return SUCCESS;
+}
